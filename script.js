7b8cad842f9f6d00c63c6d7c7414fc65552b0113
nwnordahl <nicolaiwnordahl@gmail.com>
2022-04-19 13:18:05 +0200

    Make search functionality

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-const img = document.querySelector("img");
-const button = document.querySelector("a");
+// Query selectors
+const searchBar = document.querySelector("input");
+const gif = document.querySelector("img");
+const searchButton = document.querySelector("a");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=blZBBX96YFZI1VZ6wLWPZn0JcZSDCP1x&s=rabbit",
-  { mode: "cors" }
-)
+// Global variables
+let searchURL =
+  "https://api.giphy.com/v1/gifs/translate?api_key=blZBBX96YFZI1VZ6wLWPZn0JcZSDCP1x&s=rabbit";
+let searchTerm;
+
+// Get gif from api
+fetch(searchURL, { mode: "cors" })
   .then((response) => response.json())
   .then((obj) => {
-    img.src = obj.data.images.original.url;
+    gif.src = obj.data.images.original.url;
   })
   .catch((error) => console.log(error));
 
 // Alternatively you can call this function, does the same as above
 async function gifFetcher() {
-  const response = await fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=blZBBX96YFZI1VZ6wLWPZn0JcZSDCP1x&s=rabbit",
-    { mode: "cors" }
-  );
+  const response = await fetch(searchURL, { mode: "cors" });
   const obj = await response.json();
 
-  img.src = obj.data.images.fixed_width.url;
+  gif.src = obj.data.images.fixed_width.url;
 }
 
-button.addEventListener("click", (e) => {
+// Event listeners
+searchBar.addEventListener("input", (e) => {
+  searchTerm = e.target.value.trim();
+
+  if (searchTerm === "") {
+    searchButton.textContent = "GIVE ME ";
+    return;
+  }
+
+  searchURL = `https://api.giphy.com/v1/gifs/translate?api_key=blZBBX96YFZI1VZ6wLWPZn0JcZSDCP1x&s=${searchTerm}`;
+  searchButton.textContent = `GIVE ME ${searchTerm.toUpperCase()}`;
+});
+
+searchBar.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    gifFetcher();
+    searchButton.textContent = `GIVE ME ANOTHER ${searchTerm.toUpperCase()}`;
+  }
+});
+
+searchButton.addEventListener("click", (e) => {
   gifFetcher();
+  searchButton.textContent = `GIVE ME ANOTHER ${searchTerm.toUpperCase()}`;
 });
