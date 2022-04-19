352d3333f97a5a6fddf1ea0af66f4ca3120fa59b
nwnordahl <nicolaiwnordahl@gmail.com>
2022-04-19 12:36:49 +0200

    Simple gif fetcher

---
+++ script.js
@@ -0,0 +1,26 @@
+const img = document.querySelector("img");
+const button = document.querySelector("a");
+
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=blZBBX96YFZI1VZ6wLWPZn0JcZSDCP1x&s=rabbit",
+  { mode: "cors" }
+)
+  .then((response) => response.json())
+  .then((obj) => {
+    img.src = obj.data.images.original.url;
+  });
+
+// Alternatively you can call this function, does the same as above
+async function gifFetcher() {
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=blZBBX96YFZI1VZ6wLWPZn0JcZSDCP1x&s=rabbit",
+    { mode: "cors" }
+  );
+  const obj = await response.json();
+
+  img.src = obj.data.images.fixed_width.url;
+}
+
+button.addEventListener("click", (e) => {
+  gifFetcher();
+});
